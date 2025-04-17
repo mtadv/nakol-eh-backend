038099d826ba06ec07e1831f630c62821bd40297
mtadv <mohamad.eltahtawy@mtadv.com>
2025-04-17 13:00:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const axios = require('axios');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 app.use(cors());
 app.use(express.json());
